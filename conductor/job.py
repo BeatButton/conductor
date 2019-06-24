d3b5d0050aca9efbd2a3cb34f76b4ab13130608d
Mitchell Ferree <mitchellferree@gmail.com>
2019-06-24 16:03:35 -0600

    Remove unused import

--- conductor/job.py
+++ conductor/job.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import asyncio
 import os
 import subprocess
-import sys
 from dataclasses import dataclass
 from datetime import date, datetime, time
 from pathlib import Path
